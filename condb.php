dc49fbbe603efa4f962f0b56cc36872977bfdabc
RahinF <s3297352@student.rmit.edu.au>
2020-07-29 14:10:26 +1000

    added connect to database, con.php renamed

---
+++ condb.php
@@ -0,0 +1,20 @@
+<?php
+
+// database login
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'admin';
+$DATABASE_NAME = 'sagadb';
+
+// Connect to the database
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+// display success or fail
+if ( mysqli_connect_errno() ) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+} 
+else {
+    echo 'Connection successful!';
+}
+
+?>
